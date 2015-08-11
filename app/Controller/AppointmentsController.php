56a0d770a8bfe9c78bdf6275037a3f99a3a744df
Gaurav Mehra <gaurav.mehra@iris-worldwide.com>
2015-08-11 14:35:28 +0530

    Commited the hotfix

--- app/Controller/AppointmentsController.php
+++ app/Controller/AppointmentsController.php
@@ -253,10 +253,8 @@ class AppointmentsController extends AppController {
         }
         
         function _notifyAdmin($post) {
-           
             $appointment = $this->Appointment->findById($this->Appointment->getLastInsertID());
             $this->Email->to = Configure::read('Config.admin_email');
-//            $this->Email->bcc = Configure::read('Config.dev_email');
             $this->Email->subject = 'Appointment Request';
             $this->Email->from = "ANZ Sibos Admin<info@sibos.anz.com>";
             $this->Email->template = 'appontment_admin'; 
